d6c3c1a07d48122bda8dbc323fafd578ae34e060
Andrew Shannon <a.shannon9@gmail.com>
2024-01-17 15:39:31 -0500

    modified check_crontab

--- check_crontab.sh
+++ check_crontab.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-if grep -q "0 0 */2 * * docker run --env-file /home/circleci/.env --network=host --mount type=bind,source=/home/circleci/jobrunner/scopuli/ganymede/,target=/usr/app --mount type=bind,source=/home/circleci/jobrunner/scopuli/,target=/root/.dbt/ ghcr.io/dbt-labs/dbt-postgres:1.5.2 run --target prod" <<< "crontab -l"; then
+target_job="0 0 */2 * * docker run --env-file /home/circleci/.env --network=host --mount type=bind,source=/home/circleci/jobrunner/scopuli/ganymede/,target=/usr/app --mount type=bind,source=/home/circleci/jobrunner/scopuli/,target=/root/.dbt/ ghcr.io/dbt-labs/dbt-postgres:1.5.2 run --target prod"
+
+crontab_content=$(crontab -l)
+
+if [[ $crontab_content == *"$target_job"* ]]; then
     echo "Correct pathing"
 else
     echo "Error, incorrect pathing"
