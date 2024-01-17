1c981eed0e0fdd308e5f642d552e787901d61ca0
Andrew Shannon <a.shannon9@gmail.com>
2024-01-17 13:33:50 -0500

    check crontab script

---
+++ check_crontab.sh
@@ -0,0 +1,5 @@
+if grep -q "0 0 */2 * * docker run --env-file /home/circleci/.env --network=host --mount type=bind,source=/home/circleci/jobrunner/scopuli/ganymede/,target=/usr/app --mount type=bind,source=/home/circleci/jobrunner/scopuli/,target=/root/.dbt/ ghcr.io/dbt-labs/dbt-postgres:1.5.2 run --target prod" <<< "crontab -l"; then
+    echo "Correct pathing"
+else
+    echo "Error, incorrect pathing"
+fi
